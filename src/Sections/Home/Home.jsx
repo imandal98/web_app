0b8029791b2dbb1abb421fa1628925a0714c2011
imandal98 <indrajeetmandal.aaa@gmail.com>
2023-10-24 22:28:51 +0530

    V_1.21

--- src/Sections/Home/Home.jsx
+++ src/Sections/Home/Home.jsx
@@ -3,18 +3,18 @@ import Banner from "../Banner/Banner";
 import Categogy from "./Category/Category";
 import Products from "../Products/Products";
 
-const Home =() => {
-    return (
+const Home = () => {
+  return (
     <div className="home">
-        <div className="main_containt">
-            <div className="layout">
-            <Categogy/>
-            <Products headingText="Popular products"/>
-            </div>
+      <div className="main_containt">
+        <div className="layout">
+          <Categogy />
+          <Products headingText="Best products" />
         </div>
-        {/* <Banner/>  */}
+      </div>
+      {/* <Banner/>  */}
     </div>
-    );
+  );
 };
 
 export default Home;
